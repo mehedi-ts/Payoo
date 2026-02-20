29f18130cbaf7d4577db5ff61c8d624ffee50a33
mehedi hasan <mehedihasan.j135@gmail.com>
2026-02-20 15:14:01 +0600

    chore: finalize login page design and script logic

---
+++ script/login.js
@@ -0,0 +1,25 @@
+const numberEl = document.getElementById("number");
+const passwordEl = document.getElementById("pin");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  const numberValue = numberEl.value.trim();
+  const pinValue = passwordEl.value.trim();
+  function inputEmpty() {
+    numberEl.value = "";
+    passwordEl.value = "";
+  }
+  if (isNaN(Number(numberValue)) || isNaN(Number(pinValue))) {
+    alert("Please provide a valid number and pin.");
+    inputEmpty();
+    return;
+  }
+  if (numberValue !== "01234567890" || pinValue !== "1234") {
+    alert("Invalid Number or Pin.");
+    inputEmpty();
+    return;
+  }
+
+  alert("login succeeded...");
+  inputEmpty();
+  window.location.assign("../home.html");
+});
